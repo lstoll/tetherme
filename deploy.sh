2319a26eda35e3c62e65e5c75f3fdd91ca8fd63a
Lincoln Stoll <lstoll@lstoll.net>
2009-06-18 05:25:17 +0100

    First cut at seemingly working site.
    --HG--
    extra : convert_revision : 9fe165dfa5b2abe71a758b70cc31608093f67d87

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rsync -avz --delete -e ssh * dh:tetherme.lstoll.net/
---
+++ sendconfig/index.php
@@ -0,0 +1,132 @@
+<?php 
+ob_start(); //Turn on output buffering 
+
+$action = $_GET['submit'];
+
+// get the apn
+$apn = $_GET['apn'];
+
+//define the receiver of the email 
+$to = $_GET['to']; 
+//define the subject of the email 
+$subject = 'Tethering Config'; 
+//create a boundary string. It must be unique 
+//so we use the MD5 algorithm to generate a random hash 
+$random_hash = md5(date('r', time())); 
+//define the headers we want passed. Note that they are separated with \r\n 
+$headers = "From: TetherMe <noreply@lstoll.net>\r\nReply-To: noreply@lstoll.net\r\n"; 
+//add boundary string and mime type specification 
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: multipart/mixed; boundary=\"".$random_hash."\"\r\n"; 
+ 
+//define the body of the message. 
+
+?>
+<? echo '<'.'?'?>xml version="1.0" encoding="UTF-8"<? echo '?'.'>'?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>PayloadContent</key>
+	<array>
+		<dict>
+			<key>PayloadContent</key>
+			<array>
+				<dict>
+					<key>DefaultsData</key>
+					<dict>
+						<key>apns</key>
+						<array>
+							<dict>
+								<key>apn</key>
+								<string><?=$apn?></string>
+								<key>type-mask</key>
+								<integer>-8</integer>
+
+							</dict>
+						</array>
+					</dict>
+					<key>DefaultsDomainName</key>
+					<string>com.apple.managedCarrier</string>
+				</dict>
+			</array>
+			<key>PayloadDescription</key>
+			<string>Provides customization of carrier Access Point Name.</string>
+			<key>PayloadDisplayName</key>
+			<string>Advanced Settings</string>
+			<key>PayloadIdentifier</key>
+			<string>net.lstoll.tetherme</string>
+			<key>PayloadOrganization</key>
+			<string>http://tetherme.lstoll.net</string>
+			<key>PayloadType</key>
+			<string>com.apple.apn.managed</string>
+			<key>PayloadUUID</key>
+			<string>6F1D509D-A494-444D-9098-F233891019E3</string>
+			<key>PayloadVersion</key>
+			<integer>1</integer>
+		</dict>
+	</array>
+	<key>PayloadDescription</key>
+	<string>Enables Tethering</string>
+	<key>PayloadDisplayName</key>
+	<string>Enable Tethering</string>
+	<key>PayloadIdentifier</key>
+	<string>net.lstoll.tetherme</string>
+	<key>PayloadOrganization</key>
+	<string>http://tetherme.lstoll.net</string>
+	<key>PayloadType</key>
+	<string>Configuration</string>
+	<key>PayloadUUID</key>
+	<string>A0670934-C558-42E1-9E80-9B8E079E9AE3</string>
+	<key>PayloadVersion</key>
+	<integer>1</integer>
+</dict>
+</plist>
+<?
+$config = ob_get_clean(); 
+//encode it with MIME base64,
+//and split it into smaller chunks
+$attachment = chunk_split(base64_encode($config));
+?>
+--<?php echo $random_hash; ?>
+
+Content-Type: text/plain; charset="iso-8859-1"
+Content-Transfer-Encoding: 7bit
+
+Open the attached file, and install it - you should then be ready to go with tethering.
+
+If you have any problems, please e-mail me at the address on the site.
+
+Enjoy!
+
+
+
+--<?php echo $random_hash; ?>
+
+Content-Type: application/x-apple-aspen-config; name="tether_config.mobileconfig"
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment
+
+<?php echo $attachment; ?>
+--<?php echo $random_hash; ?>
+
+<?php 
+//copy current buffer contents into $message variable and delete current output buffer 
+$message = ob_get_clean(); 
+
+if ($action == "Send") {
+  //send the email 
+  $mail_sent = @mail( $to, $subject, $message, $headers ); 
+  //if the message is sent successfully print "Mail sent". Otherwise print "Mail failed" 
+  $mail_sent ? header( 'Location: /?message=sent' ) : header( 'Location: /?message=failed' ) ;
+}
+else if ($action == "Download") {
+  // send the file
+  header('Content-type: application/x-apple-aspen-config');
+  // It will be called downloaded.pdf
+  header('Content-Disposition: attachment; filename="tetherme.mobileconfig"');
+  echo $config;
+}
+else {
+  echo 'No action specified';
+}
+?>

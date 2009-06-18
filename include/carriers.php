196527286e3474013b871c3fc95f6e06906ae3a3
Lincoln Stoll <lstoll@lstoll.net>
2009-06-18 19:20:30 +0100

    Add Saudi Telecom
    --HG--
    extra : convert_revision : 2399493453834545ee6e84c91f74a6c4497262f2

--- include/carriers.php
+++ include/carriers.php
@@ -8,6 +8,7 @@ $carriers = array(
   'auvodafone' => array('name' => 'AU - Vodafone', 'apn' => 'vfinternet.au', 'username' => '', 'password' => ''),
   'auvirgin' => array('name' => 'AU - Virgin', 'apn' => 'VirginInternet', 'username' => '', 'password' => ''),
   'chswisscom' => array('name' => 'CH - Swisscom', 'apn' => 'gprs.swisscom.ch', 'username' => '', 'password' => ''),
+  'sastc' => array('name' => 'SA - STC (Saudi Telecom)', 'apn' => 'jawalnet.com.sa', 'username' => '', 'password' => ''),
   'ukthree' => array('name' => 'UK - Three', 'apn' => 'three.co.uk', 'username' => '', 'password' => ''),
   'uko2monthly' => array('name' => 'UK - O2 (Pay Monthly)', 'apn' => 'idata.o2.co.uk', 'username' => 'vertigo', 'password' => 'password'),
   'uk02payandgo' => array('name' => 'UK - O2 (Pay and Go)', 'apn' => 'payandgo.o2.co.uk', 'username' => 'vertigo', 'password' => 'password'),

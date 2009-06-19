8449e9f42daf23d59f66a58f9fd04cc586385b2f
Lincoln Stoll <lstoll@lstoll.net>
2009-06-19 17:36:36 +0100

    Add handler for index and send, and static CSS dir
    --HG--
    extra : convert_revision : b9a8ddc3047aa789d53d2b6d57113c84cbc5915d

--- main.py
+++ main.py
@@ -1,37 +1,20 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-
-
-
 import wsgiref.handlers
-
 
 from google.appengine.ext import webapp
 
-
-class MainHandler(webapp.RequestHandler):
-
+class IndexHandler(webapp.RequestHandler):
   def get(self):
     self.response.out.write('Hello world!')
+    
+    
+class SendHandler(webapp.RequestHandler):
+  def get(self):
+    self.response.out.write('Send!')
 
 
+# Run the app
 def main():
-  application = webapp.WSGIApplication([('/', MainHandler)],
+  application = webapp.WSGIApplication([('/', IndexHandler), ('/sendconfig/', SendHandler)],
                                        debug=True)
   wsgiref.handlers.CGIHandler().run(application)
 

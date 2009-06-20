dc453a62a1a09175f3bf21cf29c5628455a25af5
Lincoln Stoll <lstoll@lstoll.net>
2009-06-20 14:42:15 +0100

    Updating to load carriers from datastore (untested)
    --HG--
    extra : convert_revision : 0b07c81252eff163f783d20944bccce1107a0082

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
-from app.data import carriers
+from app.models import *
 
 def index(request):
   message =  request.GET.get('message', False)
@@ -12,5 +12,6 @@ def index(request):
   #$message = $_GET['message'];
   #$message_type = $_GET['message_type'];
   #$_GET['manual_apn'] == 'true' ? $manual_apn = true : $manual_apn = false;
-  c = {'form': form, 'message': message, 'message_type': message_type}
+  c = {'form': form, 'message': message, 'message_type': message_type,
+    'carriers': Carrier.all().order("name").fetch(1000)}
   return render_to_response('index.html', c)

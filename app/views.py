2104c936173a2ba6a8d0a896fda0ae2472a08861
Lincoln Stoll <lstoll@lstoll.net>
2009-06-20 14:07:58 +0100

    Hooked up the index page code and forms, with message handling
    --HG--
    extra : convert_revision : 5fef6d7c3bba2f5c3e929cd8bbdc8c1ee9fc7e99

--- app/views.py
+++ app/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
+from app.data import carriers
 
 def index(request):
-  return render_to_response('index.html')
+  message =  request.GET.get('message', False)
+  message_type =  request.GET.get('message_type', '')
+  if  request.GET.get('manual_apn', '') == 'true':
+    form = '_manual_apn_form.html'
+  else:
+    form = '_carrier_select_form.html'
+  #$message = $_GET['message'];
+  #$message_type = $_GET['message_type'];
+  #$_GET['manual_apn'] == 'true' ? $manual_apn = true : $manual_apn = false;
+  c = {'form': form, 'message': message, 'message_type': message_type}
+  return render_to_response('index.html', c)
